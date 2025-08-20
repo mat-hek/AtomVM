software-mansion-labs/FissionVM

Add file:native_name_encoding/0 (#93)
These changes are made under both the "Apache 2.0" and the "GNU Lesser
General
Public License 2.1 or later" license terms (dual license).

SPDX-License-Identifier: Apache-2.0 OR LGPL-2.1-or-later

Implemented file:native_name_encoding/0 nif.
Copied PRIMITIVE_UTF8_CHECK from
https://github.com/erlang/otp/blob/OTP-26.0.2/erts/emulator/sys/common/erl_sys_common_misc.c.
- I am open for better ideas.

src/libAtomVM/nifs.c
@@ -104,6 +104,7 @@ static term nif_binary_part_3(Context *ctx, int argc, term argv[]);
 static term nif_binary_split(Context *ctx, int argc, term argv[]);
 static term nif_binary_replace(Context *ctx, int argc, term argv[]);
 static term nif_prim_file_get_cwd_0(Context *ctx, int argc, term argv[]);
+static term nif_file_native_name_encoding(Context *ctx, int argc, term argv[]);
 static term nif_calendar_system_time_to_universal_time_2(Context *ctx, int argc, term argv[]);
 static term nif_os_getenv_1(Context *ctx, int argc, term argv[]);
 static term nif_erlang_delete_element_2(Context *ctx, int argc, term argv[]);
@@ -286,6 +287,12 @@ static const struct Nif prim_file_get_cwd_nif =
 {
     .base.type = NIFFunctionType,
     .nif_ptr = nif_prim_file_get_cwd_0
+};
+
+static const struct Nif file_native_name_encoding_nif =
+{
+    .base.type = NIFFunctionType,
+    .nif_ptr = nif_file_native_name_encoding
 };
 
 static const struct Nif make_ref_nif =
@@ -5969,6 +5976,27 @@ static term nif_prim_file_get_cwd_0(Context *ctx, int argc, term argv[])
     term_put_tuple_element(result_tuple, 0, OK_ATOM);
     term_put_tuple_element(result_tuple, 1, result);
     return result_tuple;
+}
+
+static bool has_env_value(const char *env_var, const char *value)
+{
+    // This doesn't work with concurrent threads setting the env
+    const char *env_value = getenv(env_var);
+    if (env_value == NULL) {
+        return false;
+    }
+    return strstr(env_value, value) != NULL;
+}
+
+static term nif_file_native_name_encoding(Context *ctx, int argc, term argv[])
+{
+    UNUSED(ctx)
+    UNUSED(argc)
+    UNUSED(argv)
+    if (has_env_value("LC_ALL", "UTF-8") || has_env_value("LC_CTYPE", "UTF-8") || has_env_value("LANG", "UTF-8")) {
+        return UTF8_ATOM;
+    }
+    return LATIN1_ATOM;
 }
 
 #ifdef WITH_ZLIB

mat-hek/AtomVM

Signed-off-by: Mateusz Front <mateusz.front@swmansion.com>

src/libAtomVM/nifs.c
@@ -99,6 +99,7 @@ static term nif_binary_part_3(Context *ctx, int argc, term argv[]);
 static term nif_binary_split(Context *ctx, int argc, term argv[]);
 static term nif_binary_replace(Context *ctx, int argc, term argv[]);
 static term nif_binary_match(Context *ctx, int argc, term argv[]);
+static term nif_file_native_name_encoding(Context *ctx, int argc, term argv[]);
 static term nif_calendar_system_time_to_universal_time_2(Context *ctx, int argc, term argv[]);
 static term nif_erlang_delete_element_2(Context *ctx, int argc, term argv[]);
 static term nif_erlang_atom_to_binary(Context *ctx, int argc, term argv[]);
@@ -275,6 +276,12 @@ static const struct Nif binary_match_nif =
 {
     .base.type = NIFFunctionType,
     .nif_ptr = nif_binary_match
+};
+
+static const struct Nif file_native_name_encoding_nif =
+{
+    .base.type = NIFFunctionType,
+    .nif_ptr = nif_file_native_name_encoding
 };
 
 static const struct Nif make_ref_nif =
@@ -5840,6 +5847,27 @@ static term nif_erlang_lists_subtract(Context *ctx, int argc, term argv[])
 
     free(cons);
     return result;
+}
+
+
+static bool env_equals(const char *env_var, const char *value)
+{
+    const char *env_value = getenv(env_var);
+    if (env_value == NULL) {
+        return false;
+    }
+    return strstr(env_value, value) != NULL;
+}
+
+static term nif_file_native_name_encoding(Context *ctx, int argc, term argv[])
+{
+    UNUSED(ctx)
+    UNUSED(argc)
+    UNUSED(argv)
+    if (env_equals("LC_ALL", "UTF-8") || env_equals("LC_CTYPE", "UTF-8") || env_equals("LANG", "UTF-8")) {
+        return UTF8_ATOM;
+    }
+    return LATIN1_ATOM;
 }
 
 #ifdef WITH_ZLIB